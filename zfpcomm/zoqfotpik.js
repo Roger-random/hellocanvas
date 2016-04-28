4acb62b1ddece4b50bd4983ec08f5f8e960b04d3
Roger Cheng <roger.random@outlook.com>
2016-04-28 15:33:57 -0700

    Blink yoyo ambient animation

--- zfpcomm/zoqfotpik.js
+++ zfpcomm/zoqfotpik.js
@@ -1,3 +1,5 @@
+// There should be a base class for all the commonalities... but I haven't figured out how to make that work in JS yet.
+
 function CircularAnimation(startIndex, length, frameRate, initialStart, restart, blockerCallback, updatedCallback) {
 	// Starting point of the animation frames
 	var startIndex = startIndex;
@@ -62,6 +64,81 @@ function CircularAnimation(startIndex, length, frameRate, initialStart, restart,
 };
 
 
+function YoyoAnimation(startIndex, length, frameRate, initialStart, restart, blockerCallback, updatedCallback) {
+	// Starting point of the animation frames
+	var startIndex = startIndex;
+
+	// Number of frames to advance startIndex
+	var length = length;
+
+	// Amount of time, in milliseconds, between advancing frames.
+	var frameRate = frameRate;
+
+	// Amount of time, in milliseconds, before starting the first frame.
+	var restart = restart;
+
+	// Callback method to check if advanceDrawFrame should hold off.
+	var blockerCallback = blockerCallback;
+
+	// Callback method to notify that a new frame is available via currentDrawFrame
+	var updatedCallback = updatedCallback;
+
+	// Current frame index. -1 when inactive.
+	var currentFrame = -1;
+
+	// Yo-yo direction. +1 for forward, -1 for backward.
+	var yoyoDirection = +1;
+
+	// Called by the timer to calculate the next frame.
+	this.advanceDrawFrame = function() {
+		// If we are currently inactive, check if we're blocked from starting.
+		if (currentFrame == -1) {
+			if (blockerCallback()) {
+				// Set a timer to check back after the equivalent of a full cycle.
+				setTimeout(this.advanceDrawFrame.bind(this), (frameRate*length) + restart);
+				return;
+			}
+		}
+
+		// Advance to next frame
+		currentFrame += yoyoDirection;
+
+		if (currentFrame <= -1 )
+		{
+			// We're at the end of the sequence - reset and restart.
+			currentFrame = -1;
+			yoyoDirection = +1;
+			setTimeout(this.advanceDrawFrame.bind(this), restart);
+
+		} else if (currentFrame >= length) {
+			// Oops - we passed the end point. Should have gone backwards instead.
+			currentFrame = length-2; 
+			yoyoDirection = -1;
+			setTimeout(this.advanceDrawFrame.bind(this), frameRate);
+		} else {
+			// Forward or backward - we're within range. 
+			// Set timer for the following frame.
+			setTimeout(this.advanceDrawFrame.bind(this), frameRate);
+		}
+
+		// Notify of this update
+		updatedCallback();
+	}
+
+	// Called by a render function to see which frame to draw. Returns -1 if the
+	// animation is currently inactive.
+	this.currentDrawFrame = function() {
+		if (currentFrame == -1) {
+			return -1;
+		} else {
+			return startIndex + currentFrame;
+		}
+	}
+
+	// Using the initialStart parameter, sets a timer to kick off advancing frames.
+	setTimeout(this.advanceDrawFrame.bind(this), initialStart);
+};
+
 // Given a canvas, renders the ZFP communication sprites into it.
 
 // Possible future configuration options: 
@@ -77,6 +154,8 @@ function ZoqFotPikComm(targetCanvas) {
 
 	var frames = new Array();
 	var animations = new Array();
+
+	// var lastTimestamp;
 	var state = 0;
 
 	// Checked by the Zoq gulp ambient animation to see if it should start
@@ -121,7 +200,7 @@ function ZoqFotPikComm(targetCanvas) {
 	// target canvas, scaling as necessary. We need to composit the sprites
 	// while in 1:1 pixel mode before scaling because otherwise we'll get
 	// visible seams introduced by the scaling sampling algorithm.
-	var drawFrame = function() {
+	var drawFrame = function(timestamp) {
 		offscreenCanvasContext = offscreenCanvas.getContext('2d');
 		offscreenCanvasContext.drawImage(frames[0].img, 0, 0);
 
@@ -139,6 +218,11 @@ function ZoqFotPikComm(targetCanvas) {
 		targetCanvasContext.drawImage(offscreenCanvas, 0, 0, targetCanvas.width, targetCanvas.height);
 
 		canvasValid = true;
+
+        // if (lastTimestamp!=undefined) {
+        //     console.log("redraw interval " + (timestamp - lastTimestamp));
+        // }
+        // lastTimestamp = timestamp;
 	}
 
 	// Once the first frame of animations (aka the background) has loaded, we
@@ -159,7 +243,7 @@ function ZoqFotPikComm(targetCanvas) {
 		animations.push(new CircularAnimation(10, 8, 1000/15, 5000, 10000, blockZoqGulp, invalidate));
 
 		// Fot blink animation
-		//animations.push(new CircularAnimation(1, 4, 1000/24, 10000, 5000, blockZoqGulp, invalidate));
+		animations.push(new YoyoAnimation(1, 4, 1000/24, 10000, 10000, blockZoqGulp, invalidate));
 
 		// Pik smoke animation
 		animations.push(new CircularAnimation(5, 5, 7000/120, 1000, 2000, blockPikSmoke, invalidate));
